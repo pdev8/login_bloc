57fc3b639842fc233ab2b1f826ce75e84b43bfb5
Paul Lee <ppauleee@gmail.com>
2018-10-27 19:56:27 -0700

    Implemented RxDart library for submit functionality

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc extends Object with Validators {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
 
   // Add data to stream
   Function(String) get changeEmail => _email.sink.add;
@@ -12,6 +13,15 @@ class Bloc extends Object with Validators {
   // Retrieve data from stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
+
+  submit() {
+    final validEmail = _email.value;
+    final validPassword = _password.value;
+
+    print('Email is $validEmail');
+    print('Password is $validPassword');
+  }
 
   dispose() {
     _email.close();
